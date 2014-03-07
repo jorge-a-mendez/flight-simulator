93edbc3a679b9bacabfa5225ab9dc4c4a247ceeb
rafars307 <rafars307@localhost>
2014-03-07 17:46:38 -0530

    Background sketch... Esto no funcionara para el juego...

--- src/game_scube/GameControl.java
+++ src/game_scube/GameControl.java
@@ -3,7 +3,21 @@ package game_scube;
 import processing.core.*;
 
 public class GameControl extends PApplet {
-
+	
+	Background b;
+	Plane p;
+	
+	public void setup(){
+		size(1000, 640, P3D);
+		b = new Background(this, 2000, new PVector(width/2, height/2-200, -2000));
+		b.set_angle(-QUARTER_PI);
+	}
+	
+	public void draw(){
+		background(0);
+		lights();
+		b.display();
+	}
 	
 	// Main to execute the Applet...
 	public static void main(String args[]) {
